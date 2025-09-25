3fc659b69dc7f7d5525bd92a5e6fd63353508433
Baim <135421543+asgarindoo@users.noreply.github.com>
2025-09-25 22:31:46 +0700

    chore: add eslint

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import eslint from 'vite-plugin-eslint'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(), 
-    react()
+    tailwindcss(),
+    react(),
+    eslint({
+      cache: false,
+      include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+      exclude: ['node_modules']
+    })
   ],
+  server: {
+    hmr: {
+      overlay: true,
+    },
+  },
 })
+
